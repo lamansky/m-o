79f24c3fa4d599c609eb775b11d5af1ae189efd6
John Lamansky <lamansky@users.noreply.github.com>
2017-12-14 09:03:32 +0100

    Fix `set` not returning Object after setting a falsey value

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const has = (mo, key) => isMap(mo) ? mo.has(key) : Object.prototype.hasOwnProper
 const hasIn = (mo, key) => isMap(mo) ? mo.has(key) : key in mo
 const get = (mo, key) => isMap(mo) ? mo.get(key) : getOwnProperty(mo, key)
 const getIn = (mo, key) => isMap(mo) ? mo.get(key) : mo[key]
-const set = (mo, key, val) => isMap(mo) ? mo.set(key, val) : (mo[key] = val) && mo
+const set = (mo, key, val) => { if (isMap(mo)) mo.set(key, val); else mo[key] = val; return mo }
 const edit = (mo, key, editor) => set(mo, key, editor(get(mo, key)))
 const del = (mo, key) => isMap(mo) ? mo.delete(key) : has(mo, key) ? delete mo[key] : false
 const reconstruct = (mo, entries = []) => isMap(mo) ? new (mo.constructor)(entries) : newObject(entries)
